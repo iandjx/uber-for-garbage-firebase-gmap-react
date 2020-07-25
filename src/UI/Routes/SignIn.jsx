af568e2664f88d10af46f2852413616aa17858d3
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-07-25 13:31:35 +0800

    Added user type during sign in

--- src/UI/Routes/SignIn.jsx
+++ src/UI/Routes/SignIn.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { useFirebase } from "react-redux-firebase";
+import { useFirebase, useFirestore } from "react-redux-firebase";
 import { useHistory } from "react-router-dom";
 
 const SignIn = () => {
   const firebase = useFirebase();
-
+  const firestore = useFirestore();
   const signInWithGoogle = () => {
     firebase
       .login({
         provider: "google",
         type: "popup",
       })
-      .then(() => {
+      .then((cred) => {
+        firestore
+          .collection("users")
+          .doc(cred.user.uid)
+          .set({ userType: "disposer" });
         history.push("/todos");
+      })
+      .catch((err) => {
+        console.log(err);
       });
   };
 
