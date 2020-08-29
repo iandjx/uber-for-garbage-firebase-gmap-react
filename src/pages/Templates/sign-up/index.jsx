37d92094da3917dfc22e17aabf35baae56215fe9
ren santiago <santiago.renren@gmail.com>
2020-08-29 18:59:31 +0800

    change the navigate link to a variable

--- src/pages/Templates/sign-up/index.jsx
+++ src/pages/Templates/sign-up/index.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 // import components
-import Block from "@Components/Block";
-import FormInput from "@Components/Form-Input";
-import HeadingText from "@Components/Heading-text";
-import HeightSpacer from "@Components/Height-Spacer";
-import Section from "@Components/Section";
-import Button from "@Components/Button";
-import CheckBox from "@Components/CheckBox";
-import Text from "@Components/Text";
-import LinkWrapper from "@Components/Link-Wrapper";
+import Block from '@Components/Block';
+import FormInput from '@Components/Form-Input';
+import HeadingText from '@Components/Heading-text';
+import HeightSpacer from '@Components/Height-Spacer';
+import Section from '@Components/Section';
+import Button from '@Components/Button';
+import CheckBox from '@Components/CheckBox';
+import Text from '@Components/Text';
+import LinkWrapper from '@Components/Link-Wrapper';
 
+const dashboardLink = '/test/disposer';
 const Wrapper = styled.div``;
 
 const AgreeWrapper = styled.div`
@@ -27,7 +28,7 @@ const SignUp = () => {
   const navigate = useNavigate();
   const onSubmit = (e) => {
     e.preventDefault();
-    navigate("/disposer");
+    navigate(dashboardLink);
   };
   return (
     <Wrapper>
@@ -46,7 +47,7 @@ const SignUp = () => {
               <AgreeWrapper>
                 <CheckBox></CheckBox>
                 <Text>
-                  I agree to the <LinkWrapper>Terms of Use</LinkWrapper> and{" "}
+                  I agree to the <LinkWrapper>Terms of Use</LinkWrapper> and{' '}
                   <LinkWrapper>Privacy Policy</LinkWrapper>.
                 </Text>
               </AgreeWrapper>
