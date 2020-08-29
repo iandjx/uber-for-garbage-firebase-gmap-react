428892dfa9f32216c24e3e41fb2a4a6c65b93814
Ian de Jesus <ian.dejesus.md@gmail.com>
2020-08-29 20:56:41 +0800

    Added styling to signin page

--- src/pages/Auth/SignIn/index.jsx
+++ src/pages/Auth/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { Grid, Typography } from '@material-ui/core';
 import React, { useEffect } from 'react';
 import { isEmpty, isLoaded, useFirebase } from 'react-redux-firebase';
 
@@ -26,39 +27,43 @@ const SignIn = () => {
   }, [profile]);
 
   return (
-    <div>
-      <StyledFirebaseAuth
-        uiConfig={{
-          signInFlow: 'popup',
-          signInSuccessUrl: '/signedIn',
-          signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-          ],
-          callbacks: {
-            signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-              firebase.handleRedirectResult(authResult).then(() => {});
-              return false;
-            },
-          },
-        }}
-        firebaseAuth={firebase.auth()}
-      />
-      <div>
-        <h2>Auth</h2>
-        {!isLoaded(auth) ? (
-          <span>Loading...</span>
-        ) : isEmpty(auth) ? (
-          <span>Not Authed</span>
-        ) : (
-          <>
-            <pre>{JSON.stringify(auth, null, 2)}</pre>
-            <pre>{JSON.stringify(profile, null, 2)}</pre>
-          </>
-        )}
-      </div>
-    </div>
+    <>
+      <Grid container style={{ marginTop: '156px' }}>
+        <Grid
+          item
+          xs={12}
+          style={{ display: 'flex', justifyContent: 'center' }}
+        >
+          {/* <img src={logo} style={{ width: '50%' }} /> */}
+        </Grid>
+
+        <Grid item xs={12} style={{ textAlign: 'center', marginTop: '16.3px' }}>
+          <Typography variant="subtitle" style={{ textAlign: 'center' }}>
+            A reliable and eco-friendly end-to-end waste management platform
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <StyledFirebaseAuth
+            uiConfig={{
+              signInFlow: 'popup',
+              signInSuccessUrl: '/signedIn',
+              signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+                firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+              ],
+              callbacks: {
+                signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+                  firebase.handleRedirectResult(authResult).then(() => {});
+                  return false;
+                },
+              },
+            }}
+            firebaseAuth={firebase.auth()}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
