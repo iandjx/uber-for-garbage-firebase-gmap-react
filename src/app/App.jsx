0b6c8e7e8b1953409f77dfb0f3e6d159aad7852b
ren santiago <santiago.renren@gmail.com>
2020-08-29 18:37:50 +0800

    remove unused components

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,29 +1,19 @@
-import { Button, Grid } from '@material-ui/core';
-import {
-  Outlet,
-  Route,
-  BrowserRouter as Router,
-  Routes,
-  useNavigate,
-} from 'react-router-dom';
-import React, { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import { isEmpty, isLoaded } from 'react-redux-firebase';
 
 import ActiveTrashRequest from '../pages/TrashRequest/ActiveTrashRequest';
-import Auth from '../pages/Auth';
+
 import Box from '@material-ui/core/Box';
 import Camera from '../common/components/Camera';
 import Dashboard from '../pages/Dashboard';
-import Header from '../common/components/Header';
+
 import ImagePreview from '../common/components/Camera/ImagePreview';
 import NewTrashRequest from '../pages/TrashRequest/NewTrashRequest';
 import OnRoutePickup from '../pages/Collector/OnRoutePickup';
 import PrivateRoute from '../common/components/PrivateComponent';
-import Register from '../pages/Auth/Register';
-import SideBar from '../common/components/SideBar';
+
 import SignIn from '../pages/Auth/SignIn';
-import Test from '../pages/Test';
-import TrashRequest from '../pages/TrashRequest';
 import TrashRequestConfirmation from '../pages/TrashRequest/TrashRequestConfirmation';
 import TrashRequestList from '../pages/Collector/TrashRequestList';
 import { useSelector } from 'react-redux';
@@ -48,6 +38,7 @@ const App = (props) => {
     if (profile.userType === 'household' && isLoaded(profile)) {
       navigate('/disposer/new-request');
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth, profile]);
 
   return (
