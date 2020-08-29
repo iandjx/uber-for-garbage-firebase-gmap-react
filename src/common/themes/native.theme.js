d341a6ed61d95bf6bfc375d1a9db80d5fda19dbe
ren santiago <santiago.renren@gmail.com>
2020-08-29 18:35:01 +0800

    added assets and themes

---
+++ src/common/themes/native.theme.js
@@ -0,0 +1,16 @@
+export const Theme = {
+    palette : {
+        primary : '#24A5A9' ,
+        secondary  :'#37bfc1' ,
+        accent: '#b0ce39' ,
+        darkerAccent : '#94b13c' ,
+        blue : '#205072',
+        appleGreen : "#8acb60" ,
+        white : "#FFFFFF"
+    } ,
+    maxWidth : '720px',
+    sidebar : {
+        width : '245px'
+    }
+};
+export default Theme;
